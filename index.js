f6ed4227e2bdd773e51ca96c9a86029f4c63a576
surachaiobotic <surachairobotic@gmail.com>
2023-11-07 17:35:03 +0700

    ...

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000; // You can change this to any port you prefer
+const PORT = 8001; // You can change this to any port you prefer
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your Node.js server!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
